ed22d0531886422f9cdbea607cee9c09435d3219
Oybek <oybekjonxayrullayevich@gmail.com>
2022-04-05 10:16:59 +0500

    slide

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-console.log([1, 2, 3, 5, 6, 5, 7]);
-console.log(new Array(1, 2, 3, 4, 7, 8, 9));
+// console.log([1, 2, 3, 5, 6, 5, 7]);
+// console.log(new Array(1, 2, 3, 4, 7, 8, 9));
 
-let x = new Array(10);
-x.fill(1, 2, 3);
-console.log(x);
+// let x = new Array(10);
+// x.fill(1, 2, 3);
+// console.log(x);
 
-let arr = new Array(10);
+// let arr = new Array(10);
 
-// fill(element,startIndex, endIndex)
+// // fill(element,startIndex, endIndex)
 
-arr.fill(5, "", 5);
-console.log(arr);
+// arr.fill(5, "", 5);
+// console.log(arr);
 
-let y = [4, 5, 6, 7, 9];
-let newY = Array.from(y, function (val, key) {
-  return val * 2;
-});
-console.log(newY);
+// let y = [4, 5, 6, 7, 9];
+// let newY = Array.from(y, function (val, key) {
+//   return val * 2;
+// });
+// console.log(newY);
 
-let arr2 = Array.from({ length: 10 }, (val, key) => key + 3);
+// let arr2 = Array.from({ length: 10 }, (val, key) => key + 3);
 
-console.log(arr2);
+// console.log(arr2);
+
+
+
+const func = async function(){
+  
+}
